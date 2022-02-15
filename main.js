a3bed9b4a2f8b2680cc0af5750293d54918a232f
helenaKuthanova <helena.kuthanova@gmail.com>
2022-02-15 18:51:48 +0100

    using classList and add/remove cssclass functions

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 // tady je místo pro náš program
 console.log("Jsem nalinkován");
+
+let nadpis = document.querySelector("h1");
+nadpis.classList.add("zluty");
+
+let ctverecek = document.querySelector("div.zluty");
+ctverecek.classList.add("ctverecek");
+
+nadpis.classList.remove("zluty");
